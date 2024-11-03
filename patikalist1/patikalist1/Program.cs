13b4c904fda19556fdd1ad793572b0803bdcad78
BerkanGH <berkankeskin28@gmail.com>
2024-11-03 15:52:09 +0300

    odev

---
+++ patikalist1/patikalist1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        // Ziyaretçilerin isimlerini içeren bir liste oluşturuyoruz.
+        List<string> ziyaretciler = new List<string>
+        {
+            "Lionel Messi",
+            "Cristiona Ronaldo",
+            "Maldini",
+            "Xavi",
+            "Neymar",
+            "Tayfur Bingöl",
+            "Ali koç"
+        };
+
+        // Liste içindeki her ismi ekrana yazdırmak için foreach döngüsü kullanıyoruz.
+        Console.WriteLine("Patika Plus Gala Gecesine Hoşgeldiniz");
+        Console.WriteLine("Davetli listemiz:");
+        int index = 1;
+        foreach (string davetlilistesi in ziyaretciler)
+        {
+            Console.WriteLine($"{index}) {davetlilistesi}");
+            index++;
+        }
+    }
+}
